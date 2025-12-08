2bfaca6d7ef904f9b10d738ce6ad90c7d7345e79
Tofique Nasir <165983601+Saifi-29@users.noreply.github.com>
2025-12-08 21:39:31 +0530

    Time: 39 ms (32.08%) | Memory: 8.5 MB (11.06%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    bool binarysearch(vector<int> &arr, int target)
+    {
+        int s = 0, e = arr.size()-1;
+        int mid;
+        while(s<=e)
+        {
+            mid = (s+e)/2;
+            if(arr[mid] == target)
+            return true;
+            else if(arr[mid]>target)
+            e = mid-1;
+            else s = mid+1;
+        }
+        return false;
+    }
+public:
+    int countTriples(int n) {
+        vector<int> arr;
+        for(int i=1;i<=n;i++)
+        {
+            arr.push_back(i*i);
+        }
+        // sort(arr.begin/(), arr.end());
+
+        int count = 0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=n;j>1;j--)
+            {
+                int num = i*i + j*j;
+                // float c = sqrt(num);
+                if(binarysearch(arr,num))
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
