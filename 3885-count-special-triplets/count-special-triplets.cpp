35b796b716630795f1c8ee40a3e90bb7cf3c7110
Tofique Nasir <165983601+Saifi-29@users.noreply.github.com>
2025-12-09 20:54:32 +0530

    Time: 923 ms (34.67%) | Memory: 428 MB (36.86%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
         const long long MOD = 1e9 + 7;
-        int n = nums.size();
+        unordered_map<int, int> right;
+        unordered_map<int, int> left;
 
-        unordered_map<int, long long> rightCount;
-        unordered_map<int, long long> leftCount;
-
-        // fill rightCount with full array
-        for (int x : nums) rightCount[x]++;
-
-        long long result = 0;
-
-        for (int j = 0; j < n; j++) {
-            int mid = nums[j];
-            rightCount[mid]--;  // j moves from right to left
-
-            long long left = leftCount[mid * 2];
-            long long right = rightCount[mid * 2];
-
-            result = (result + left * right) % MOD;
-
-            leftCount[mid]++;
+        for(int i=0;i<nums.size();i++)
+        {
+            right[nums[i]]++;
         }
+        int res = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            int mid = nums[i];
+            right[mid]--;
+            int l = left[mid*2];
+            int r = right[mid*2];
 
-        return result % MOD;
+            res = (res%MOD+(l%MOD*r%MOD));
+            left[mid]++;
+        }
+        return res;
     }
 };
