185570381afb684c3c393e5d4fad4bb9221f4e98
Tofique Nasir <165983601+Saifi-29@users.noreply.github.com>
2025-12-13 22:54:22 +0530

    Time: 8 ms (46.78%) | Memory: 69.7 MB (46.35%) - LeetSync

---
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    bool isValidCode(const string &s) {
+    if (s.empty()) return false;
+    for (char c : s) {
+        if (!isalnum(c) && c != '_') {
+            return false;
+        }
+    }
+    return true;
+}
+
+public:
+    vector<string> validateCoupons(vector<string>& code,
+                                   vector<string>& businessLine,
+                                   vector<bool>& isActive) {
+        vector<string> order = {"electronics", "grocery", "pharmacy",
+                                "restaurant"};
+        unordered_map<string, vector<string>> mp;
+        unordered_set<string> validBusiness(order.begin(), order.end());
+
+        int n = code.size();
+        for (int i = 0; i < n; i++) {
+            if (isActive[i] && isValidCode(code[i]) &&
+                validBusiness.count(businessLine[i])) {
+
+                mp[businessLine[i]].push_back(code[i]);
+            }
+        }
+
+        vector<string> result;
+        for (const string& b : order) {
+            auto& codes = mp[b];
+            sort(codes.begin(), codes.end());
+            result.insert(result.end(), codes.begin(), codes.end());
+        }
+
+        return result;
+    }
+};
